68ebbc17a3f7d3be4f815f7935a210b666915744
uni51 <unieye51@gmail.com>
2021-03-27 17:33:46 +0900

    6-3: 商品検索フォームを作ろう

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 Route::middleware('auth')
     ->group(function () {
         Route::get('items/{item}/buy', function () { return "商品購入画面";})->name('item.buy');
-        
+
         Route::get('sell', 'SellController@showSellForm')->name('sell');
         Route::post('sell', 'SellController@sellItem')->name('sell');
     });
