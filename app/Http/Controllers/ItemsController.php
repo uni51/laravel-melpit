1b5ebdcb28d1855d5851fa0c6ee3152d8ee81dec
uni51 <unieye51@gmail.com>
2021-03-29 09:30:58 +0900

    6-4: 商品を検索する処理を作ろう

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -9,8 +9,33 @@ class ItemsController extends Controller
 {
     public function showItems(Request $request)
     {
-        // derByRawメソッドを使って、出品中の商品を先に、購入済みの商品を後に表示している
-        $items = Item::orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
+        $query = Item::query();
+
+        // カテゴリで絞り込み
+        //　Requestインスタンスのfilledメソッドで、パラメータが指定されているかを調べることが可能
+        if ($request->filled('category')) {
+            list($categoryType, $categoryID) = explode(':', $request->input('category'));
+
+            if ($categoryType === 'primary') {
+                $query->whereHas('secondaryCategory', function ($query) use ($categoryID) {
+                    $query->where('primary_category_id', $categoryID);
+                });
+            } else if ($categoryType === 'secondary') {
+                $query->where('secondary_category_id', $categoryID);
+            }
+        }
+
+        // キーワードで絞り込み
+        if ($request->filled('keyword')) {
+            $keyword = '%' . $this->escape($request->input('keyword')) . '%';
+            $query->where(function ($query) use ($keyword) {
+                $query->where('name', 'LIKE', $keyword);
+                $query->orWhere('description', 'LIKE', $keyword);
+            });
+        }
+
+        // orderByRawメソッドを使って、出品中の商品を先に、購入済みの商品を後に表示している
+        $items = $query->orderByRaw( "FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')" )
             ->orderBy('id', 'DESC')
             ->paginate(1);
 
@@ -22,5 +47,14 @@ class ItemsController extends Controller
     {
         return view('items.item_detail')
             ->with('item', $item);
+    }
+
+    private function escape(string $value)
+    {
+        return str_replace(
+            ['\\', '%', '_'],
+            ['\\\\', '\\%', '\\_'],
+            $value
+        );
     }
 }
